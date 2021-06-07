7531507c5d14d065b7bb146437465f12eb42124e
Sugimomoto <sugimomoto@gmail.com>
2021-06-07 23:42:27 +0900

    Added User class

--- HashmapSample.java
+++ HashmapSample.java
@@ -2,26 +2,33 @@ import java.util.Hashtable;
 
 public class HashmapSample{
     public static void main(String[] args){
-      System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
 
-      Hashtable<Integer,String> members = new Hashtable<Integer,String>();
+        Hashtable<Integer,String> members = new Hashtable<Integer,String>();
 
-      members.put(1, "Kazuya");
-      members.put(2, "Haru");
-      members.put(3, "Hogehoge");
+        members.put(1, "Kazuya");
+        members.put(2, "Haru");
+        members.put(3, "Hogehoge");
 
-      String member = members.get(1);
-      System.out.println(member);
+        String member = members.get(1);
+        System.out.println(member);
 
-      // containsKey containsValue でチェック
-      System.out.println(members.containsKey(1));
-      System.out.println(members.containsValue("Kazuya"));
+        // containsKey containsValue でチェック
+        System.out.println(members.containsKey(1));
+        System.out.println(members.containsValue("Kazuya"));
 
-      members.remove(1);
+        members.remove(1);
 
-      for (Integer key : members.keySet()) {
-          System.out.println(members.get(key));
-      }
+        for (Integer key : members.keySet()) {
+            System.out.println(members.get(key));
+        }
 
+        Hashtable<Integer,User> users = new Hashtable<Integer,User>();
+
+        users.put(1,new User(1,"Hello"));
+        users.put(2,new User(2,"World"));
+
+        System.out.println(users.get(1).Name);      
     }
  }
+
---
+++ User.java
@@ -0,0 +1,9 @@
+public class User{
+
+    public User (Integer id, String name){
+        this.Id = id;
+        this.Name = name;
+    }
+    public Integer Id;
+    public String Name;
+}
